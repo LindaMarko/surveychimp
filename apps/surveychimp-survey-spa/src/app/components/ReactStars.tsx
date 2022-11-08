5582cbe86c356f6adf451795cd2dde4593d4d6b4
tejav27 <tejav2727@gmail.com>
2022-11-08 11:17:38 +0100

    Add react stars component file

---
+++ apps/surveychimp-survey-spa/src/app/components/ReactStars.tsx
@@ -0,0 +1,11 @@
+import ReactStars from 'react-stars'
+  
+ const ReactStarsComponent = () => {
+   return <ReactStars
+  count={5}
+  size={24}
+  color2={'#ffd700'} />
+ };
+ 
+ export default ReactStarsComponent;
+ 
